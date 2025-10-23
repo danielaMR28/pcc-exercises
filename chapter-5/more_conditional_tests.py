b360a281c57981af21cd4a95e1179e2a94f3d823
danielaMR28 <daniela.mejia.rivas@gmail.com>
2025-10-23 16:10:20 -0600

    Exercises Chapter 5.1-2

---
+++ chapter-5/more_conditional_tests.py
@@ -0,0 +1,95 @@
+car = 'subaru'
+print("\nIs car == 'subaru' ? I predict True.")
+print(car == 'subaru')
+
+print("Is car == 'audi' ? I predict False.")
+print(car == 'audi')
+
+
+color = 'red'
+print("\nIs color == 'blue' ? I predict False.")
+print(color == 'blue')
+
+print("Is color == 'red' ? I predict True.")
+print(color == 'red')
+
+
+flower = 'rose'
+print("\nIs flower == 'rose' ? I predict True.")
+print(flower == 'rose')
+
+print("Is flower == 'sunflower' ? I predict False.")
+print(flower == 'sunflower')
+
+
+girl = 'Maria'
+print("\nIs girl == 'Lola' ? I predict False.")
+print(girl == 'Lola')
+
+print("Is girl == 'Maria' ? I predict True.")
+print(girl == 'Maria')
+
+
+pet = 'cat'
+print("\nIs pet == 'cat' ? I predict True.")
+print(pet == 'cat')
+
+print("Is pet == 'dog' ? I predict False.")
+print(pet == 'dog')
+
+
+print("\nMORE CONDITIONAL TESTS")
+
+
+word = 'sun'
+print("\nIs word == 'sun' ? I predict True.")
+print(word == 'sun')
+
+print("Is word != 'sun' ? I predict False.")
+print(word != 'sun')
+
+
+word2 = 'beach'
+print("\nIs word2 == 'BEACH'.lower() ? I predict True.")
+print(word2 == 'BEACH'.lower())
+
+print("Is word2 == 'BEACH' ? I predict False.")
+print(word2 == 'BEACH')
+
+
+apples = 3
+print("\nIs apples >= 3 ? I predict True.")
+print(apples >= 3)
+
+print("Is apples < 2 ? I predict False.")
+print(apples < 2)
+
+
+animal1 = "fish"
+animal2 = "bear"
+print("\nIs animal1 != 'fish' and animal2 != 'bear' ? I predict False.")
+print(animal1 != "fish" and animal2 != "bear")
+
+print("Is animal1 == 'turtle' or animal2 == 'bear' ? I predict True.")
+print(animal1 == "turtle" or animal2 == "bear")
+
+
+names = ["Maria", "Lola", "Carmen"]
+print("\nIs Maria in names ? I predict True.")
+print("Maria" in names)
+
+print("Is Juan in names ? I predict False.")
+print("Juan" in names)
+
+
+fruits = ["apple", "peach", "mango"]
+print("\nIs apple not in fruits ? I predict False.")
+print("apple" not in fruits)
+
+print("Is banana not in fruits ? I predict True.")
+print("banana" not in fruits)
+
+
+
+
+
