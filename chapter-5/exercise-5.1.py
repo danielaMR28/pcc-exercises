26a685c825631ceb3a083613ee308510fc9fbbe1
danielaMR28 <daniela.mejia.rivas@gmail.com>
2025-10-23 15:38:38 -0600

    Exercise 5-1 Conditional Tests!

---
+++ chapter-5/exercise-5.1.py
@@ -0,0 +1,34 @@
+car = 'subaru'
+print("\nIs car == 'subaru' ? I predict True.")
+print(car == 'subaru')
+
+print("\nIs car == 'audi' ? I predict False.")
+print(car == 'audi')
+
+color = 'red'
+print("\nIs color == 'blue' ? I predict False.")
+print(color == 'blue')
+
+print("\nIs color == 'red' ? I predict True.")
+print(color == 'red')
+
+flower = 'rose'
+print("\nIs flower == 'rose' ? I predict True.")
+print(flower == 'rose')
+
+print("\nIs flower == 'sunflower' ? I predict False.")
+print(flower == 'sunflower')
+
+girl = 'Maria'
+print("\nIs girl == 'Lola' ? I predict False.")
+print(girl == 'Lola')
+
+print("\nIs girl == 'Maria' ? I predict True.")
+print(girl == 'Maria')
+
+pet = 'cat'
+print("\nIs pet == 'cat' ? I predict True.")
+print(pet == 'cat')
+
+print("\nIs pet == 'dog' ? I predict False.")
+print(pet == 'dog')
