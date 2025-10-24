eee797b52f25edd7a631ac4812b77355b3ab6def
danielaMR28 <daniela.mejia.rivas@gmail.com>
2025-10-23 22:05:32 -0600

    Exercise alien colors

---
+++ chapter-5/alien_colors_1.py
@@ -0,0 +1,9 @@
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("You just earn 5 points")
+
+alien_color2 = 'red'
+
+if alien_color2 == 'green':
+    print("You just earn 5 points")
--- chapter-5/alien_colors_2.py
+++ chapter-5/alien_colors_2.py
@@ -1,9 +1,13 @@
-alien_color = 'green'
+alien_color = 'yellow'
 
 if alien_color == 'green':
-    print("You just earn 5 points")
+    print("You just earn 5 points for shooting the alien.")
+else:
+    print("You just earn 10 points.")
 
-alien_color2 = 'red'
+alien_color2 = 'green'
 
 if alien_color2 == 'green':
-    print("You just earn 5 points")
+    print("You just earn 5 points for shooting the alien.")
+else:
+    print("You just earn 10 points.")
