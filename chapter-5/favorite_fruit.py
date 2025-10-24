ad1e0ce93448c4d5e28e35b3e29177518e1fedbf
danielaMR28 <daniela.mejia.rivas@gmail.com>
2025-10-23 22:22:30 -0600

    Exercise favorite fruit

---
+++ chapter-5/favorite_fruit.py
@@ -0,0 +1,16 @@
+favorite_fruits = ["kiwi", "peach", "strawberry"]
+
+if 'kiwi' in favorite_fruits:
+    print("You really like kiwis!")
+
+if 'strawberry' in favorite_fruits:
+    print("You really like strawberries!")
+
+if 'apple' in favorite_fruits:
+    print("You really like apples!")
+
+if 'mango' in favorite_fruits:
+    print("You really like mangoes!")
+
+if 'peach' in favorite_fruits:
+    print("You really like peaches!")
