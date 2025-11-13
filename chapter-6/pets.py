d6201075ebb23c117f121da6695299e4c94e7621
danielaMR28 <daniela.mejia.rivas@gmail.com>
2025-11-12 22:39:34 -0600

    Chapter 6

---
+++ chapter-6/pets.py
@@ -0,0 +1,10 @@
+pet1 = {'kind_of_animal' : 'Dog', 'owner_name' : 'Juan'}
+pet2 = {'kind_of_animal' : 'Cat', 'owner_name' : 'Pedro'}
+pet3 = {'kind_of_animal' : 'Fish', 'owner_name' : 'Maira'}
+pet4 = {'kind_of_animal' : 'Bird', 'owner_name' : 'Carla'}
+pet5 = {'kind_of_animal' : 'Hamster', 'owner_name' : 'Brenda'}
+
+pets = [pet1, pet2, pet3, pet4, pet5]
+
+for pet in pets:
+    print(pet)
