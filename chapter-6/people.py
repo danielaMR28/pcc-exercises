dfe34713cd865e7931ee8cdd55a3739693936d99
danielaMR28 <daniela.mejia.rivas@gmail.com>
2025-11-12 22:34:17 -0600

    Chapter 6

---
+++ chapter-6/people.py
@@ -0,0 +1,8 @@
+person = {'first_name' : 'Ana', 'last_name' : 'Perez', 'age' : 22 , 'city' : 'CDMX'}
+person2 = {'first_name' : 'Juan', 'last_name' : 'Reza', 'age' : 24 , 'city' : 'Puebla'}
+person3 = {'first_name' : 'Daniela', 'last_name' : 'Rivas', 'age' : 23 , 'city' : 'Guadalajara'}
+
+people = [person, person2, person3]
+
+for persona in people:
+    print(persona)
