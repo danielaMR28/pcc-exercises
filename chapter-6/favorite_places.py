a93ca4dbde5860627785011857adc82fbf009a2d
danielaMR28 <daniela.mejia.rivas@gmail.com>
2025-11-12 22:48:07 -0600

    Chapter 6

---
+++ chapter-6/favorite_places.py
@@ -0,0 +1,4 @@
+favorite_places = {"Dani": "Beach, Italy, Home", "Juan": "Germany, Argentina, Chile", "Yina": "France, New York, Beach"}
+
+for place in favorite_places:
+    print(place, ":", favorite_places[place])
