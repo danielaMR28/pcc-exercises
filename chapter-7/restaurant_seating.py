9043d28b605750cd7ed7a0d19dd6818e59b9ff16
danielaMR28 <daniela.mejia.rivas@gmail.com>
2025-11-13 23:29:29 -0600

    Chapter 7

---
+++ chapter-7/restaurant_seating.py
@@ -0,0 +1,6 @@
+number_people = input("How many people are in their dinner group?")
+
+if int(number_people) > 8:
+    print("You'll have to wait for a table")
+else:
+    print("Your table is ready")
